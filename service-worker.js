beba3b3bd4a16ac9e32b486c1a55480db2fc76d3
Jamie Bexten <jbexten89@gmail.com>
2026-08-13 18:36:03 -0400

    Fix: use overflow-x: clip on body only (html scroll container was blocked); SW v10

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Paycheck Allocator - minimal cache-first service worker */
-const CACHE = 'paycheck-allocator-v9';
+const CACHE = 'paycheck-allocator-v10';
 const ASSETS = [
   './',
   './index.html',
