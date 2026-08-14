90d483426c05d0df66f36a5a8e85d7df4002247b
Jamie Bexten <jbexten89@gmail.com>
2026-08-13 22:20:41 -0400

    iOS PWA polish: black-translucent status bar, splash SVG, safe-area-inset-top; SW v11

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Paycheck Allocator - minimal cache-first service worker */
-const CACHE = 'paycheck-allocator-v10';
+const CACHE = 'paycheck-allocator-v11';
 const ASSETS = [
   './',
   './index.html',
