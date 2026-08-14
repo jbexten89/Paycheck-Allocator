e6a5480a8978e468b079dc0417df991216adcb12
Jamie Bexten <jbexten89@gmail.com>
2026-08-14 05:53:30 -0400

    Sync: add 6-digit OTP code verification so PWA can sign in without magic-link crossing storage boundary; SW v12

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Paycheck Allocator - minimal cache-first service worker */
-const CACHE = 'paycheck-allocator-v11';
+const CACHE = 'paycheck-allocator-v12';
 const ASSETS = [
   './',
   './index.html',
