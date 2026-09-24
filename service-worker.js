debe85248bf75afb3455f55ba6301783e63d9abf
Jamie Bexten <jbexten89@gmail.com>
2026-09-24 13:33:52 -0400

    Revert "Settings: cap set-aside at income; roll excess to next pay"
    This reverts commit 956e62582993053a5d36aeeac7e97777a2cd5443.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Paycheck Allocator - minimal cache-first service worker */
-const CACHE = 'paycheck-allocator-v83';
+const CACHE = 'paycheck-allocator-v82';
 const ASSETS = [
   './',
   './index.html',
